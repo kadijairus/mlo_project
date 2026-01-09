32ae44bea2d9bb84063500181a2cce1749966101
hameleon-ed <haimaned@gmail.com>
2026-01-09 15:16:45 +0200

    - Add Hydra configuration - Update training script for hyperparameter tuning. - Add basic integration Hydra configuration with W&B

--- src/mlo_group_project/train.py
+++ src/mlo_group_project/train.py
@@ -3,22 +3,24 @@ import typer
 from pathlib import Path
 from mlo_group_project.model import BreastCancerModel
 
+# handling configurations and experimenting
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import wandb
 
 #Setting up Typer app , For HP tuning 
 app = typer.Typer()
 @app.command()
 
-def train_model(
-    epochs: int = 100,
-    lr: float = 0.001,
-    batch_size: int = 64,
-    processed_dir: Path = Path("data/processed"),
-    model_save_path: Path = Path("models/model.pth"),
-    
-):
-        print(f" Starting training: epochs={epochs}, lr={lr}, batch_size={batch_size}")
-        print(f" Loading data from: {processed_dir}")
+@hydra.main(config_path="cnf", config_name="config", version_base=None)
+def train_model(cnf: DictConfig):
+    # Convert Hydra config to a standard dictionary
+    wandb_config = OmegaConf.to_container(
+        cnf, resolve=True, throw_on_missing=True
+        )
+    print(f" Starting training: epochs={cnf.hp.epochs}, lr={cnf.hp.lr}, batch_size={cnf.hp.batch_size}")
+    print(f" Loading data from: {cnf.paths.processed_dir}")
+    print(f" Save model to: {cnf.paths.model_save_path}")
         
-
-
-
+if __name__  == "__main__":
+    train_model()
