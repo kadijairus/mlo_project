22f87b71e41e8fcb084b89928635d3ccc22c4dc2
Farnood Khordepaz <165167248+kfarnood@users.noreply.github.com>
2026-01-09 15:07:19 +0100

    Refactor model to BreastCancerModel with ANN structure
    Refactored the model to implement a BreastCancerModel with a more complex architecture using ReLU activations and dropout layers.
    #kfarnood

--- src/mlo_group_project/model.py
+++ src/mlo_group_project/model.py
@@ -1,16 +1,27 @@
-from torch import nn
 import torch
+import torch.nn as nn
 
-class Model(nn.Module):
-    """Just a dummy model to show how to structure your code"""
-    def __init__(self):
-        super().__init__()
-        self.layer = nn.Linear(1, 1)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layer(x)
+#We just use a simple ANN , with 30 input nodes , all activations are ReLU for simplicity, Having some dropout to counter overfit
+class BreastCancerModel(nn.Module):
+    def __init__(self, input_shape: int):
+        super(BreastCancerModel, self).__init__()
+        
+        
+        self.network = nn.Sequential(
+            
+            nn.Linear(input_shape, 64),
+            nn.ReLU(),
+            nn.Dropout(0.3),  
+            
+            
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            
+            
+            nn.Linear(32, 1)
+        )
 
-if __name__ == "__main__":
-    model = Model()
-    x = torch.rand(1)
-    print(f"Output shape of model: {model(x).shape}")
+    def forward(self, x):
+        return self.network(x)
